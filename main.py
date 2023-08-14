8adb3b8109544da18748423ec122113f08aae727
PeterWanjihia <peter.wanjihia@student.moringaschool.com>
2023-08-14 21:53:45 +0300

    made changes to cater for the non_alphabet characters

--- main.py
+++ main.py
@@ -47,22 +47,62 @@
         
 # p_letters = ''.join(map(str,[item[0] for item in V_decryption(e_indices,k_indices)]))        
     
+# import string
+
+# def text_indices(text, alphabet):
+#     return [alphabet.index(char) for char in text]
+
+# def key_indices(key, text, alphabet):
+#     key = (key * (len(text) // len(key))) + key[:len(text) % len(key)]
+#     return [alphabet.index(char) for char in key]
+
+# def V_encryption(p_indices, k_indices, alphabet):
+#     e_indices = [(p + k) % 26 for p, k in zip(p_indices, k_indices)]
+#     return [alphabet[index] for index in e_indices]
+
+# def V_decryption(e_indices, k_indices, alphabet):
+#     d_indices = [(e - k) % 26 for e, k in zip(e_indices, k_indices)]
+#     return [alphabet[index] for index in d_indices]
+
+# def main():
+#     alphabet = list(string.ascii_lowercase)
+#     plaintext = input('Enter the text:')
+#     key = input('Enter the key:')
+    
+#     p_indices = text_indices(plaintext, alphabet)
+#     k_indices = key_indices(key, plaintext, alphabet)
+    
+#     encrypted_letters = V_encryption(p_indices, k_indices, alphabet)
+#     encrypted_text = ''.join(encrypted_letters)
+    
+#     e_indices = text_indices(encrypted_text, alphabet)
+    
+#     decrypted_letters = V_decryption(e_indices, k_indices, alphabet)
+#     decrypted_text = ''.join(decrypted_letters)
+    
+#     print("Original Text:", plaintext)
+#     print("Encrypted Text:", encrypted_text)
+#     print("Decrypted Text:", decrypted_text)
+
+# if __name__ == "__main__":
+#     main()
+
 import string
 
 def text_indices(text, alphabet):
-    return [alphabet.index(char) for char in text]
+    return [alphabet.index(char) if char in alphabet else char for char in text]
 
 def key_indices(key, text, alphabet):
     key = (key * (len(text) // len(key))) + key[:len(text) % len(key)]
-    return [alphabet.index(char) for char in key]
+    return [alphabet.index(char) if char in alphabet else char for char in key]
 
 def V_encryption(p_indices, k_indices, alphabet):
-    e_indices = [(p + k) % 26 for p, k in zip(p_indices, k_indices)]
-    return [alphabet[index] for index in e_indices]
+    e_indices = [(p + k) % 26 if isinstance(p, int) else p for p, k in zip(p_indices, k_indices)]
+    return [alphabet[index] if isinstance(index, int) else index for index in e_indices]
 
 def V_decryption(e_indices, k_indices, alphabet):
-    d_indices = [(e - k) % 26 for e, k in zip(e_indices, k_indices)]
-    return [alphabet[index] for index in d_indices]
+    d_indices = [(e - k) % 26 if isinstance(e, int) else e for e, k in zip(e_indices, k_indices)]
+    return [alphabet[index] if isinstance(index, int) else index for index in d_indices]
 
 def main():
     alphabet = list(string.ascii_lowercase)
@@ -86,4 +126,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
